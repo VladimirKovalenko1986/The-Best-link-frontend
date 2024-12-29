6ca6e5574e541feb22dfecd0cea8aa18d75d706d
Volodymyr Kovalenko <dante77758@gmail.com>
2024-12-29 18:59:23 +0200

    add trim from setFieldValue

--- src/components/UserFormFormik/UserFormFormik.jsx
+++ src/components/UserFormFormik/UserFormFormik.jsx
@@ -4,8 +4,12 @@ import * as Yup from "yup";
 import css from "./UserFormFormik.module.css";
 
 const UserSchema = Yup.object().shape({
-  username: Yup.string().min(3, "Min 3 charts!!!").required("Is required!!!"),
+  username: Yup.string()
+    .trim()
+    .min(3, "Min 3 charts!!!")
+    .required("Is required!!!"),
   email: Yup.string()
+    .trim()
     .email("Must be a valid email!!!")
     .required("Is required!!!"),
   role: Yup.string()
@@ -38,50 +42,74 @@ export default function UserFormFormik({ onAdd }) {
       validationSchema={UserSchema}
       onSubmit={handleSubmit}
     >
-      <div className={css.conteiner}>
-        <h2 className={css.title}>Form Formik</h2>
-        <Form className={css.form}>
-          <div className={css.group}>
-            <label htmlFor={usernameId}>UserName:</label>
-            <Field name="username" id={usernameId} />
-            <ErrorMessage
-              className={css.error}
-              name="username"
-              component="span"
-            />
-          </div>
+      {({ setFieldValue }) => (
+        <div className={css.conteiner}>
+          <h2 className={css.title}>Form Formik</h2>
+          <Form className={css.form}>
+            <div className={css.group}>
+              <label htmlFor={usernameId}>UserName:</label>
+              <Field
+                name="username"
+                id={usernameId}
+                onBlur={(e) => setFieldValue("username", e.target.value.trim())}
+              />
+              <ErrorMessage
+                className={css.error}
+                name="username"
+                component="span"
+              />
+            </div>
 
-          <div className={css.group}>
-            <label htmlFor={emailId}>Email:</label>
-            <Field type="email" id={emailId} name="email" />
-            <ErrorMessage className={css.error} name="email" component="span" />
-          </div>
+            <div className={css.group}>
+              <label htmlFor={emailId}>Email:</label>
+              <Field
+                type="email"
+                id={emailId}
+                name="email"
+                onBlur={(e) => setFieldValue("email", e.target.value.trim())}
+              />
+              <ErrorMessage
+                className={css.error}
+                name="email"
+                component="span"
+              />
+            </div>
 
-          <div className={css.group}>
-            <label htmlFor={roleId}>Role:</label>
-            <Field as="select" name="role" id={roleId}>
-              <option value="guest">Guest</option>
-              <option value="user">User</option>
-              <option value="admin">Admin</option>
-            </Field>
-            <ErrorMessage className={css.error} name="role" component="span" />
-          </div>
+            <div className={css.group}>
+              <label htmlFor={roleId}>Role:</label>
+              <Field as="select" name="role" id={roleId}>
+                <option value="guest">Guest</option>
+                <option value="user">User</option>
+                <option value="admin">Admin</option>
+              </Field>
+              <ErrorMessage
+                className={css.error}
+                name="role"
+                component="span"
+              />
+            </div>
 
-          <div className={css.group}>
-            <label htmlFor={commentId}>Comment:</label>
-            <Field as="textarea" name="comment" id={commentId} />
-            <ErrorMessage
-              className={css.error}
-              name="comment"
-              component="span"
-            />
-          </div>
+            <div className={css.group}>
+              <label htmlFor={commentId}>Comment:</label>
+              <Field
+                as="textarea"
+                name="comment"
+                id={commentId}
+                onBlur={(e) => setFieldValue("comment", e.target.value.trim())}
+              />
+              <ErrorMessage
+                className={css.error}
+                name="comment"
+                component="span"
+              />
+            </div>
 
-          <button className={css.btn} type="submit">
-            Submite
-          </button>
-        </Form>
-      </div>
+            <button className={css.btn} type="submit">
+              Submit
+            </button>
+          </Form>
+        </div>
+      )}
     </Formik>
   );
 }
